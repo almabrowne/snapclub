62429bbd8170b3df8eb6f127f8abe7624087975e
almabrowne <42240075+almabrowne@users.noreply.github.com>
2018-08-09 12:48:09 +0100

    My first commit, YAY!

---
+++ script.js
@@ -0,0 +1,23 @@
+// Add your code below...
+const myName = "Bruno"
+const mySurname = "Jurkovski"
+const myFullName = myName + " " + mySurname
+
+const number1 = 2
+const number2 = 8
+const sumNumbers = number1 + number2
+
+function flipCoin(threshold) {
+	const randomNumber = Math.random()
+	if (randomNumber > threshold) {
+		alert("You won, " + myFullName)
+	} else {
+		alert("You lost, " + myFullName)
+	}
+}
+
+function slideimage() {
+	$('.myProfile').slideUp ();
+	$('.myProfile').slideDown();
+}
+
